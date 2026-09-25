c2003ed20340d5af9199da54f81a0b1ae4731e2d
Hyris <132802433+Hirxs@users.noreply.github.com>
2026-09-25 16:45:22 -0600

    Delete src directory

--- src/BP/scripts/fabulous_furnished/math/randomNumFunctions.js
+++
@@ -1,8 +0,0 @@
-export function randomWholeNum(min, max) {
-    const minInt = Math.ceil(min);
-    const maxInt = Math.floor(max);
-    return Math.floor(Math.random() * (maxInt - minInt + 1)) + minInt;
-}
-export function randomNum(min, max) {
-    return Math.random() * (max - min) + min;
-}
